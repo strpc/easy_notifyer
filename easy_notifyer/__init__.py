c12801bf2238e0b8cf4aa88c59763069c23a05c2
strpc <renat.kruchinskij@gmail.com>
2020-12-15 23:27:21 +0300

    init модуля

---
+++ easy_notifyer/__init__.py
@@ -0,0 +1,5 @@
+from easy_notifyer.telegram import Telegram, TelegramAsync, telegram_reporter
+from easy_notifyer.mail import Mailer
+
+
+__all__ = ["Telegram", "TelegramAsync", "telegram_reporter", "Mailer"]
