cce0c24c2441b18a9118ccebb6553649afca5f63
strpc <renat.kruchinskij@gmail.com>
2020-12-17 00:13:58 +0300

    изменил сигнатуру методов post

--- easy_notifyer/clients.py
+++ easy_notifyer/clients.py
@@ -1,17 +1,20 @@
-from httpx import AsyncClient, Client
+from typing import Any, Dict
+
+from httpx import AsyncClient, Client, Response
 
 
 class AsyncRequests:
     def __init__(self):
         self._client = AsyncClient()
 
-    async def post(self, *args, **kwargs):
-        self._client.post(*args, **kwargs)
+    async def post(self, *, url: str, params: Dict, body: Dict, data: Any) -> Response:
+        async with self._client as client:
+            return await client.post(url=url, params=params, json=body, data=data)
 
 
 class Requests:
     def __init__(self):
         self._client = Client()
 
-    def post(self, *args, **kwargs):
-        return self._client.post(*args, **kwargs)
+    def post(self, *, url: str, params: Dict, body: Dict, data: Any) -> Response:
+        return self._client.post(url=url, params=params, json=body, data=data)
