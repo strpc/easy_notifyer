b35510331222ad1b9fefeb380b4f3df2a42991fd
strpc <renat.kruchinskij@gmail.com>
2020-12-17 00:11:19 +0300

    дописал асинхронный декоратор, асинхронный клиент телеграма, переписал переменные окружения на Env-объект. исправил сигнатуры методов-клиентов

--- easy_notifyer/telegram.py
+++ easy_notifyer/telegram.py
@@ -1,14 +1,15 @@
-import os
+import asyncio
+import functools
 import traceback
-from functools import wraps
-from typing import Optional, Union, List
+from typing import Union, List, Callable, Any, Dict, Optional
 
-from easy_notifyer.clients import AsyncRequests, Requests
+from easy_notifyer.clients import AsyncRequests, Requests, Response
+from easy_notifyer.env import Env
 from easy_notifyer.utils import make_report, get_telegram_creds
 
 
 class TelegramBase:
-    API_BASE_URL = os.getenv('EASY_NOTIFYER_API_BASE_URL', 'https://api.telegram.org/')
+    API_BASE_URL = Env.EASY_NOTIFYER_API_BASE_URL
     API_BASE_URL = API_BASE_URL[:-1] if API_BASE_URL.endswith('/') else API_BASE_URL
 
     def __init__(self, token: str, chat_id: Union[int, List[int]]):
@@ -22,8 +23,44 @@ class TelegramAsync(TelegramBase):
         super().__init__(token, chat_id)
         self._client = AsyncRequests()
 
-    def send_report(self):
-        pass
+    async def _send_post(
+            self,
+            *,
+            method_api: str,
+            params: Optional[Dict] = None,
+            body: Optional[Dict] = None,
+            data: Optional[Any] = None
+    ) -> Response:
+        return await self._client.post(
+            url=self._request_url + method_api,
+            params=params,
+            body=body,
+            data=data,
+        )
+
+    async def send_message(self, msg: str, **kwargs):
+        method_api = 'sendMessage'
+        chat_ids = [self._chat_id] if isinstance(self._chat_id, int) else self._chat_id
+
+        for chat_id in chat_ids:
+            body = {
+                'chat_id': chat_id,
+                'text': msg,
+            }
+            if kwargs.get('parse_mode'):
+                body['parse_mode'] = kwargs['parse_mode']
+            if kwargs.get('disable_web_page_preview'):
+                body['disable_web_page_preview'] = True
+            await self._send_post(method_api=method_api, body=body)
+
+    async def send_report(self, tback: str, func_name: Optional[str] = None):
+        report = await self._run_sync(make_report, tback, func_name)
+        await self.send_message(report)
+
+    @staticmethod
+    async def _run_sync(func: Callable, *args, **kwargs):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(None, functools.partial(func, *args, **kwargs))
 
 
 class Telegram(TelegramBase):
@@ -37,9 +74,20 @@ class Telegram(TelegramBase):
         super().__init__(token, chat_id)
         self._client = Requests()
 
-    def _send_request(self, *, method_api: str, method_req: str, **kwargs):
-        method = getattr(self._client, method_req)
-        method(url=self._request_url + method_api, **kwargs)
+    def _send_post(
+            self,
+            *,
+            method_api: str,
+            params: Optional[Dict] = None,
+            body: Optional[Dict] = None,
+            data: Optional[Any] = None,
+    ) -> Response:
+        return self._client.post(
+            url=self._request_url + method_api,
+            params=params,
+            body=body,
+            data=data,
+        )
 
     def send_message(self, msg: str, **kwargs):
         method_api = 'sendMessage'
@@ -54,20 +102,25 @@ class Telegram(TelegramBase):
                 body['parse_mode'] = kwargs['parse_mode']
             if kwargs.get('disable_web_page_preview'):
                 body['disable_web_page_preview'] = True
-            self._send_request(method_api=method_api, method_req='post', json=body, **kwargs)
+            self._send_post(method_api=method_api, body=body)
 
     def send_report(self, tback: str, func_name: Optional[str] = None):
         report = make_report(tback, func_name)
         self.send_message(report)
 
 
-def telegram_reporter(*, token: Optional[str] = None, chat_id: Optional[int] = None, **params):
+def telegram_reporter(
+        *,
+        token: Optional[str] = None,
+        chat_id: Optional[int] = None,
+        **params
+):
     exceptions = params.get('exceptions', Exception)
     if token is None or chat_id is None:
         token, chat_id = get_telegram_creds()
 
     def decorator(func):
-        @wraps(func)
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
@@ -77,5 +130,30 @@ def telegram_reporter(*, token: Optional[str] = None, chat_id: Optional[int] = N
                 bot = Telegram(token=token, chat_id=chat_id)
                 bot.send_report(tback, func_name)
                 raise exc
-        return wrapper()
+        return wrapper
+    return decorator
+
+
+def async_telegram_reporter(
+        *,
+        token: Optional[str] = None,
+        chat_id: Optional[int] = None,
+        **params
+):
+    exceptions = params.get('exceptions', Exception)
+    if token is None or chat_id is None:
+        token, chat_id = get_telegram_creds()
+
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except exceptions as exc:
+                func_name = func.__name__
+                tback = traceback.format_exc()
+                bot = TelegramAsync(token=token, chat_id=chat_id)
+                await bot.send_report(tback, func_name)
+                raise exc
+        return wrapper
     return decorator
