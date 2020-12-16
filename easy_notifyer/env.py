3be6bbba1de29a4c76da60dc13386e38723d112f
strpc <renat.kruchinskij@gmail.com>
2020-12-17 00:13:23 +0300

    добавил объект Env с переменными окружения

---
+++ easy_notifyer/env.py
@@ -0,0 +1,13 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Env:
+    EASY_NOTIFYER_TELEGRAM_TOKEN: str = os.getenv('EASY_NOTIFYER_TELEGRAM_TOKEN')
+    EASY_NOTIFYER_TELEGRAM_CHAT_ID: str = os.getenv('EASY_NOTIFYER_TELEGRAM_CHAT_ID')
+    EASY_NOTIFYER_PROJECT_NAME: str = os.getenv('EASY_NOTIFYER_PROJECT_NAME')
+    EASY_NOTIFYER_DATE_FORMAT: str = os.getenv('EASY_NOTIFYER_DATE_FORMAT',
+                                               '%Y-%m-%d %H:%M:%S')
+    EASY_NOTIFYER_API_BASE_URL: str = os.getenv('EASY_NOTIFYER_API_BASE_URL',
+                                                'https://api.telegram.org/')
