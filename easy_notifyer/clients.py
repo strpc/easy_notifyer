348f13f3402702dc09ca79d93779c5442d116b1d
strpc <renat.kruchinskij@gmail.com>
2020-12-17 02:47:13 +0300

    добавил вариант отправки файлов

--- easy_notifyer/clients.py
+++ easy_notifyer/clients.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union, IO
 
 from httpx import AsyncClient, Client, Response
 
@@ -7,14 +7,30 @@ class AsyncRequests:
     def __init__(self):
         self._client = AsyncClient()
 
-    async def post(self, *, url: str, params: Dict, body: Dict, data: Any) -> Response:
+    async def post(
+            self,
+            *,
+            url: str,
+            params: Dict,
+            body: Dict,
+            data: Any,
+            files: Union[IO[str], IO[bytes], str, bytes],
+    ) -> Response:
         async with self._client as client:
-            return await client.post(url=url, params=params, json=body, data=data)
+            return await client.post(url=url, params=params, json=body, data=data, files=files)
 
 
 class Requests:
     def __init__(self):
         self._client = Client()
 
-    def post(self, *, url: str, params: Dict, body: Dict, data: Any) -> Response:
-        return self._client.post(url=url, params=params, json=body, data=data)
+    def post(
+            self,
+            *,
+            url: str,
+            params: Dict,
+            body: Dict,
+            data: Any,
+            files: Union[IO[str], IO[bytes], str, bytes],
+    ) -> Response:
+        return self._client.post(url=url, params=params, json=body, data=data, files=files)
