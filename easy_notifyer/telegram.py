d949106948875f0db79043c4f4d1f4a7fe7e873b
strpc <renat.kruchinskij@gmail.com>
2020-12-18 03:30:00 +0300

    дописал отправку файла. и прикрепление файла к отчету

--- easy_notifyer/telegram.py
+++ easy_notifyer/telegram.py
@@ -1,13 +1,14 @@
-import asyncio
 import functools
 import traceback
+import uuid
+from datetime import datetime
 from io import BytesIO
-from typing import IO, Any, Callable, Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 from easy_notifyer.clients import AsyncRequests, Requests, Response
 from easy_notifyer.env import Env
 from easy_notifyer.report import Report
-from easy_notifyer.utils import get_telegram_creds
+from easy_notifyer.utils import get_telegram_creds, run_sync
 
 
 class TelegramBase:
@@ -31,8 +32,8 @@ class TelegramAsync(TelegramBase):
             method_api: str,
             params: Optional[Dict] = None,
             body: Optional[Dict] = None,
-            data: Optional[Any] = None,
-            files: Optional[Union[IO[str], IO[bytes], str, bytes]] = None
+            data: Optional[Dict] = None,
+            files: Optional[Dict] = None
     ) -> Response:
         return await self._client.post(
             url=self._request_url + method_api,
@@ -57,43 +58,41 @@ class TelegramAsync(TelegramBase):
                 body['disable_web_page_preview'] = True
             await self._send_post(method_api=method_api, body=body)
 
-    async def send_attach(self, attach: Any, text: Optional[str] = None, **kwargs):
+    async def send_attach(
+            self,
+            msg: str,
+            attach: Union[bytes, str, Tuple],
+            filename: Optional[str] = None
+    ):
         method_api = 'sendDocument'
         chat_ids = [self._chat_id] if isinstance(self._chat_id, int) else self._chat_id
+        filename = filename or uuid.uuid4().hex
 
-        if isinstance(attach, bytes):
-            file = BytesIO(attach)
+        files = {}
+        if isinstance(attach, tuple):
+            files['document'] = attach
+        elif isinstance(attach, bytes):
+            files['document'] = (filename, BytesIO(attach))
         else:
-            file = BytesIO(attach.encode())
+            files['document'] = (filename, BytesIO(attach.encode()))
 
-        body = {}
-        if text is not None:
-            body['caption'] = text
-        if kwargs.get('parse_mode'):
-            body['parse_mode'] = kwargs['parse_mode']
-        if kwargs.get('disable_web_page_preview'):
-            body['disable_web_page_preview'] = True
+        params = {}
+        if msg is not None:
+            params['caption'] = msg
 
         for chat_id in chat_ids:
-            body['chat_id'] = chat_id
-            await self._send_post()
+            params['chat_id'] = chat_id
+            await self._send_post(method_api=method_api, params=params, files=files)
 
-
-    async def send_report(self, tback: str, **kwargs):
-        func_name = kwargs.get('func_name')
-        header = kwargs.get('header')
-        as_attached = kwargs.get('as_attached')
-        report: Report = await self._run_sync(Report, tback, func_name, header, as_attached)
-
-        if as_attached is not None:
-            await self.send_attach(report.attach, report.report)
+    async def send_report(self, report: Report, **kwargs):
+        if report.attach is not None:
+            date = datetime.now().replace(microsecond=0).strftime(
+                Env.EASY_NOTIFYER_FILENAME_DT_FORMAT
+            )
+            filename = f"{date}.txt"
+            await self.send_attach(msg=report.report, attach=report.attach, filename=filename)
         else:
-            await self.send_message(report.report)
-
-    @staticmethod
-    async def _run_sync(func: Callable, *args, **kwargs):
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, functools.partial(func, *args, **kwargs))
+            await self.send_message(report.report, **kwargs)
 
 
 class Telegram(TelegramBase):
@@ -113,8 +112,8 @@ class Telegram(TelegramBase):
             method_api: str,
             params: Optional[Dict] = None,
             body: Optional[Dict] = None,
-            data: Optional[Any] = None,
-            files: Optional[Union[IO[str], IO[bytes], str, bytes]] = None
+            data: Optional[Dict] = None,
+            files: Optional[Dict] = None,
     ) -> Response:
         return self._client.post(
             url=self._request_url + method_api,
@@ -139,31 +138,64 @@ class Telegram(TelegramBase):
                 body['disable_web_page_preview'] = True
             self._send_post(method_api=method_api, body=body)
 
-    def send_attach(self, report: Report):
-        pass
+    def send_attach(
+            self,
+            msg: str,
+            attach: Union[bytes, str, Tuple],
+            filename: Optional[str] = None
+    ):
+        method_api = 'sendDocument'
+        chat_ids = [self._chat_id] if isinstance(self._chat_id, int) else self._chat_id
+        filename = filename or uuid.uuid4().hex
 
-    def send_report(self, tback: str, **kwargs):
-        func_name = kwargs.get('func_name')
-        header = kwargs.get('header')
-        as_attached = kwargs.get('as_attached')
-        report = Report(tback, func_name, header, as_attached)
-        if as_attached is not None:
-            self.send_attach(report)
+        files = {}
+        if isinstance(attach, tuple):
+            files['document'] = attach
+        elif isinstance(attach, bytes):
+            files['document'] = (filename, BytesIO(attach))
         else:
-            self.send_message(report.report)
+            files['document'] = (filename, BytesIO(attach.encode()))
+
+        params = {}
+        if msg is not None:
+            params['caption'] = msg
+
+        for chat_id in chat_ids:
+            params['chat_id'] = chat_id
+            self._send_post(method_api=method_api, params=params, files=files)
+
+    def send_report(self, report: Report, **kwargs):
+        if report.attach is not None:
+            date = datetime.now().replace(microsecond=0).strftime(
+                Env.EASY_NOTIFYER_FILENAME_DT_FORMAT
+            )
+            filename = f"{date}.txt"
+            self.send_attach(msg=report.report, attach=report.attach, filename=filename)
+        else:
+            self.send_message(report.report, **kwargs)
+
+
+def _report_maker(
+        *,
+        tback: str,
+        func_name: Optional[str] = None,
+        header: Optional[str] = None,
+        as_attached: bool = False,
+) -> Report:
+    return Report(tback, func_name, header, as_attached)
 
 
 def telegram_reporter(
         *,
         token: Optional[str] = None,
         chat_id: Optional[Union[List[int], int]] = None,
+        exceptions,
+        header: Optional[str] = None,
+        as_attached: bool = False,
         **params
 ):
-    exceptions = params.get('exceptions', Exception)
     if token is None or chat_id is None:
         token, chat_id = get_telegram_creds()
-    as_attached = params.get('as_attached', False)
-    header = params.get('header')
 
     def decorator(func):
         @functools.wraps(func)
@@ -174,7 +206,13 @@ def telegram_reporter(
                 func_name = func.__name__
                 tback = traceback.format_exc()
                 bot = Telegram(token=token, chat_id=chat_id)
-                bot.send_report(tback, func_name=func_name, header=header, as_attached=as_attached)
+                report = _report_maker(
+                    tback=tback,
+                    func_name=func_name,
+                    header=header,
+                    as_attached=as_attached,
+                )
+                bot.send_report(report, **params)
                 raise exc
         return wrapper
     return decorator
@@ -184,13 +222,13 @@ def async_telegram_reporter(
         *,
         token: Optional[str] = None,
         chat_id: Optional[Union[List[int], int]] = None,
+        exceptions,
+        header: Optional[str] = None,
+        as_attached: bool = False,
         **params
 ):
-    exceptions = params.get('exceptions', Exception)
     if token is None or chat_id is None:
         token, chat_id = get_telegram_creds()
-    as_attached = params.get('as_attached', False)
-    header = params.get('header')
 
     def decorator(func):
         @functools.wraps(func)
@@ -200,8 +238,15 @@ def async_telegram_reporter(
             except exceptions as exc:
                 func_name = func.__name__
                 tback = traceback.format_exc()
+                report = await run_sync(
+                    _report_maker,
+                    tback=tback,
+                    func_name=func_name,
+                    header=header,
+                    as_attached=as_attached,
+                )
                 bot = TelegramAsync(token=token, chat_id=chat_id)
-                await bot.send_report(tback, func_name=func_name, header=header, as_attached=as_attached)
+                await bot.send_report(report, **params)
                 raise exc
         return wrapper
     return decorator
