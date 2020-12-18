f59872edd9f214dc3c3c57474628ef888d5599b0
strpc <renat.kruchinskij@gmail.com>
2020-12-18 03:28:11 +0300

    добавил формат файлнейма

--- easy_notifyer/env.py
+++ easy_notifyer/env.py
@@ -9,5 +9,7 @@ class Env:
     EASY_NOTIFYER_PROJECT_NAME: str = os.getenv('EASY_NOTIFYER_PROJECT_NAME')
     EASY_NOTIFYER_DATE_FORMAT: str = os.getenv('EASY_NOTIFYER_DATE_FORMAT',
                                                '%Y-%m-%d %H:%M:%S')
+    EASY_NOTIFYER_FILENAME_DT_FORMAT: str = os.getenv('EASY_NOTIFYER_FILENAME_DT_FORMAT',
+                                                   '%Y-%m-%d %H_%M_%S')
     EASY_NOTIFYER_API_BASE_URL: str = os.getenv('EASY_NOTIFYER_API_BASE_URL',
                                                 'https://api.telegram.org/')
