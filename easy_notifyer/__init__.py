9955e4e06aa7227dd3f2c8bc928ce554458bb353
strpc <renat.kruchinskij@gmail.com>
2020-12-20 14:15:37 +0300

    перенес декораторы-обработчики в отдельный модуль. подрефакторил чутка

--- easy_notifyer/__init__.py
+++ easy_notifyer/__init__.py
@@ -1,6 +1,6 @@
+from easy_notifyer.handlers import async_telegram_reporter, telegram_reporter
 from easy_notifyer.mail import Mailer
-from easy_notifyer.telegram import (Telegram, TelegramAsync,
-                                    async_telegram_reporter, telegram_reporter)
+from easy_notifyer.telegram import Telegram, TelegramAsync
 
 __all__ = [
     "Mailer",
