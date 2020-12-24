9987ba436006f0a1e313a08fcf2009f43a07215f
strpc <renat.kruchinskij@gmail.com>
2020-12-24 03:35:45 +0300

    разделил переменные окружения для каждого "сервиса" отдельно

--- easy_notifyer/env.py
+++ easy_notifyer/env.py
@@ -2,8 +2,13 @@ import os
 from dataclasses import dataclass
 
 
+__all__ = [
+    'Env',
+]
+
+
 @dataclass
-class Env:
+class EnvTelegram:
     EASY_NOTIFYER_PROJECT_NAME: str = os.getenv('EASY_NOTIFYER_PROJECT_NAME')
     EASY_NOTIFYER_DATE_FORMAT: str = os.getenv('EASY_NOTIFYER_DATE_FORMAT',
                                                '%Y-%m-%d %H:%M:%S')
@@ -13,3 +18,19 @@ class Env:
     EASY_NOTIFYER_TELEGRAM_CHAT_ID: str = os.getenv('EASY_NOTIFYER_TELEGRAM_CHAT_ID')
     EASY_NOTIFYER_TELEGRAM_API_URL: str = os.getenv('EASY_NOTIFYER_TELEGRAM_API_URL',
                                                     'https://api.telegram.org/')
+
+
+@dataclass
+class EnvMailer:
+    EASY_NOTIFYER_MAILER_HOST: str = os.getenv('EASY_NOTIFYER_MAILER_HOST')
+    EASY_NOTIFYER_MAILER_PORT: str = os.getenv('EASY_NOTIFYER_MAILER_PORT')
+    EASY_NOTIFYER_MAILER_LOGIN: str = os.getenv('EASY_NOTIFYER_MAILER_LOGIN')
+    EASY_NOTIFYER_MAILER_PASSWORD: str = os.getenv('EASY_NOTIFYER_MAILER_PASSWORD')
+    EASY_NOTIFYER_MAILER_FROM: str = os.getenv('EASY_NOTIFYER_MAILER_FROM')
+    EASY_NOTIFYER_MAILER_TO: str = os.getenv('EASY_NOTIFYER_MAILER_TO')
+    EASY_NOTIFYER_MAILER_SSL: bool = os.getenv('EASY_NOTIFYER_MAILER_SSL', False) in (True, 'True')
+
+
+@dataclass
+class Env(EnvTelegram, EnvMailer):
+    ...
