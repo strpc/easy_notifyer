9159aa6b074aa661f52988f26830293b1f23a4a7
strpc <renat.kruchinskij@gmail.com>
2020-12-24 03:34:37 +0300

    добавил докстринги, убрал лишний функционал

--- easy_notifyer/utils.py
+++ easy_notifyer/utils.py
@@ -1,11 +1,16 @@
 import asyncio
 import functools
-from typing import Callable, List, Tuple, Union
+from typing import Any, Callable, List, Tuple
 
 from easy_notifyer.env import Env
 
 
-def get_telegram_creds() -> Union[Tuple[str, int], Tuple[str, List[int]]]:
+def get_telegram_creds() -> Tuple[str, List[int]]:
+    """
+    Get telegram creds from environment variable
+    Returns:
+        Tuple[token(str), List[chat_id(int), ...]]
+    """
     token = Env.EASY_NOTIFYER_TELEGRAM_TOKEN
     chat_id = Env.EASY_NOTIFYER_TELEGRAM_CHAT_ID
     error = EnvironmentError(f"Telegram token or chat_id is not found. token={token}, "
@@ -17,11 +22,18 @@ def get_telegram_creds() -> Union[Tuple[str, int], Tuple[str, List[int]]]:
         chat_id = [int(i) for i in chat_id if i]
     except ValueError as exc:
         raise error from exc
-    if len(chat_id) == 1:
-        return token, chat_id[0]
     return token, chat_id
 
 
-async def run_sync(func: Callable, *args, **kwargs):
+async def run_sync(func: Callable, *args, **kwargs) -> Any:
+    """
+    Run sync func in async code in thread pool.
+    Args:
+        func(callable): func to run
+        *args:
+        **kwargs:
+    Returns:
+        same as func
+    """
     loop = asyncio.get_event_loop()
     return await loop.run_in_executor(None, functools.partial(func, *args, **kwargs))
