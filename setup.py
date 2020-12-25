949de27dfaf60f82debdec15e1cbc36b57e92cc3
strpc <renat.kruchinskij@gmail.com>
2020-12-25 03:02:50 +0300

    перенес версию в init-файл

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
+import re
 from itertools import chain
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 EXTRAS = {
@@ -14,15 +15,32 @@ def read(filename: str):
     return open(os.path.join(os.path.dirname(__file__), filename)).read().strip()
 
 
+def read_version():
+    """Read version from __init__.py"""
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(
+        os.path.dirname(__file__),
+        'easy_notifyer',
+        '__init__.py'
+    )
+    with open(init_py) as file:
+        for line in file:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        raise RuntimeError('Cannot find version in easy_notifyer/__init__.py')
+
+
 setup(
     name='easy_notifyer',
-    version='0.0.3',
+    version=read_version(),
     description='Easy bug reporter for small projects or Sentry on minimums. Async support.',
     author='strpc',
     url='https://github.com/strpc/easy_notifyer',
     long_description=read('README.md'),
     long_description_content_type="text/markdown",
     download_url='https://pypi.python.org/pypi/easy-notifyer',
+    packages=find_packages(include=['easy_notifyer']),
     install_requires=EXTRAS['telegram'],
     extras_require={
         'telegram': EXTRAS['telegram'],
