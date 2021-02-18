9de616f798fb4258a6315057e698f28c2ec9544f
strpc <renat.kruchinskij@gmail.com>
2021-02-18 20:56:15 +0300

    вернул TelegramAsync

--- easy_notifyer/__init__.py
+++ easy_notifyer/__init__.py
@@ -1,6 +1,6 @@
 from easy_notifyer.handlers import mailer_reporter, telegram_reporter
 from easy_notifyer.mailer import Mailer
-from easy_notifyer.telegram import Telegram
+from easy_notifyer.telegram import Telegram, TelegramAsync
 from easy_notifyer.exceptions import ConfigError
 
 
@@ -11,6 +11,7 @@ __all__ = [
     'ConfigError',
     "Mailer",
     "Telegram",
+    'TelegramAsync',
     "mailer_reporter",
     "telegram_reporter",
 ]
