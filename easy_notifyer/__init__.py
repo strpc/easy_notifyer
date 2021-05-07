e110940d6085834915f07128f4f89ee86a52d09c
strpc <renat.kruchinskij@gmail.com>
2021-05-08 01:45:55 +0300

    поправил импорты

--- easy_notifyer/__init__.py
+++ easy_notifyer/__init__.py
@@ -1,7 +1,7 @@
+from easy_notifyer.clients.mailer import Mailer
+from easy_notifyer.clients.telegram import Telegram, TelegramAsync
 from easy_notifyer.handlers.mailer import mailer_reporter
 from easy_notifyer.handlers.telegram import telegram_reporter
-from easy_notifyer.mailer import Mailer
-from easy_notifyer.telegram import Telegram, TelegramAsync
 
 
 __version__ = "0.1.3"
