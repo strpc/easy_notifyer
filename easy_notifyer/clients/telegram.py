c5f7381e5e84df2090d58ff156715755ea631e3e
strpc <renat.kruchinskij@gmail.com>
2021-05-20 01:28:27 +0300

    убрал абстрактный класс. убрал ретерн response. поправил докстринги

--- easy_notifyer/clients/telegram.py
+++ easy_notifyer/clients/telegram.py
@@ -1,38 +1,15 @@
 # pylint: disable=too-few-public-methods
 import logging
 import uuid
-from abc import ABC, abstractmethod
 from io import BytesIO
 from typing import BinaryIO, Dict, List, Optional, Tuple, Union
 from urllib.error import HTTPError
 
-from easy_notifyer.clients.requests import AsyncRequests, Requests, Response
+from easy_notifyer.clients.requests import AsyncRequests, Requests
 from easy_notifyer.utils import run_in_threadpool
 
 
 logger = logging.getLogger(__name__)
-
-
-class ITelegram(ABC):
-    @abstractmethod
-    def send_message(
-        self,
-        msg: str,
-        disable_notification: bool = False,
-        disable_web_page_preview: bool = False,
-    ) -> None:
-        ...
-
-    @abstractmethod
-    def send_attach(
-        self,
-        attach: Union[bytes, str, BinaryIO, Tuple[str, Union[BinaryIO, bytes]]],
-        *,
-        msg: Optional[str] = None,
-        filename: Optional[str] = None,
-        disable_notification: bool = False,
-    ) -> None:
-        ...
 
 
 class TelegramBase:
@@ -47,8 +24,8 @@ class TelegramBase:
     ) -> None:
         """
         Args:
-            token(str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
-            chat_id(int, str, list): Chat ids for send message.
+            token (str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
+            chat_id (int, str, list): Chat ids for send message.
         """
         self._token = token
         self._chat_ids = [chat_id] if isinstance(chat_id, (int, str)) else chat_id
@@ -63,13 +40,14 @@ class TelegramBase:
         attach: Union[bytes, str, BinaryIO, Tuple[str, Union[BinaryIO, bytes]]],
         filename: Optional[str] = None,
     ) -> Dict:
-        """
-        Preparation of attach for sending to telegram
+        """Preparation of attach for sending to telegram
+
         Args:
-            attach(bytes, str, binaryio, tuple): attach to send
-            filename: filename
+            attach (bytes, str, binaryio, tuple): attach to send
+            filename (str, optional): filename
+
         Returns:
-            {'document': (filename, b'file)}
+            Dict: {'document': (filename, b'file)}
         """
         filename = filename or uuid.uuid4().hex
         files = {}
@@ -84,7 +62,7 @@ class TelegramBase:
         return files
 
 
-class TelegramAsync(ITelegram, TelegramBase):
+class TelegramAsync(TelegramBase):
     """Async client for telegram"""
 
     def __init__(
@@ -92,12 +70,12 @@ class TelegramAsync(ITelegram, TelegramBase):
         *,
         token: str,
         chat_id: Union[List[int], int, List[str], str],
-        api_url: Optional[str],
+        api_url: Optional[str] = None,
     ) -> None:
         """
         Args:
-            token(str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
-            chat_id(int, str, list): Chat ids for send message.
+            token (str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
+            chat_id (int, str, list): Chat ids for send message.
             api_url (str, optional): telegram api url.
         """
         super().__init__(token=token, chat_id=chat_id, api_url=api_url)
@@ -111,21 +89,18 @@ class TelegramAsync(ITelegram, TelegramBase):
         params: Optional[Dict] = None,
         body: Optional[Dict] = None,
         files: Optional[Dict] = None,
-    ) -> Optional[Response]:
-        """
-        Send async post request.
-        Args:
-            method_api(str): method of api telegram
-            headers(dict, optional): headers for of request.
-            params(dict, optional): params of request.
-            body(dict, optional): body of request.
-            files(dict, optional): files of request in format ('filename.txt', b'filedata').
+    ) -> None:
+        """Send async post request.
 
-        Returns:
-            instance of Response.
+        Args:
+            method_api (str): method of api telegram
+            headers (dict, optional): headers for of request.
+            params (dict, optional): params of request.
+            body (dict, optional): body of request.
+            files (dict, optional): files of request in format ('filename.txt', b'filedata').
         """
         try:
-            response = await self._client.post(
+            await self._client.post(
                 url=self._base_api_url + method_api,
                 headers=headers,
                 params=params,
@@ -136,8 +111,6 @@ class TelegramAsync(ITelegram, TelegramBase):
             logger.exception("Error. %s", error)
         except Exception:  # noqa
             logger.error("Send message to telegram error.")
-        else:
-            return response
 
     async def send_message(
         self,
@@ -145,12 +118,12 @@ class TelegramAsync(ITelegram, TelegramBase):
         disable_notification: bool = False,
         disable_web_page_preview: bool = False,
     ) -> None:
-        """
-        Send message.
+        """Send message.
+
         Args:
-            msg(str): text of message.
-            disable_notification(bool): True to disable notification of message.
-            disable_web_page_preview(bool): True to disable web preview for links.
+            msg (str): text of message.
+            disable_notification (bool): True to disable notification of message.
+            disable_web_page_preview (bool): True to disable web preview for links.
         """
         method_api = "sendMessage"
 
@@ -178,12 +151,10 @@ class TelegramAsync(ITelegram, TelegramBase):
 
         Args:
             attach (bytes, str, tuple): file to send. if tuple, then
-            ('filename.txt', b'text of file').
-
+                ('filename.txt', b'text of file').
             msg (str, optional): text of message.
-            filename(str, optional): filename if attach is string or bytes.
-
-            disable_notification(bool): True to disable notification of message.
+            filename (str, optional): filename if attach is string or bytes.
+            disable_notification (bool): True to disable notification of message.
         """
         method_api = "sendDocument"
         files = await run_in_threadpool(self._prepare_attach, attach=attach, filename=filename)
@@ -199,7 +170,7 @@ class TelegramAsync(ITelegram, TelegramBase):
             await self._send_post(method_api=method_api, params=params, files=files)
 
 
-class Telegram(ITelegram, TelegramBase):
+class Telegram(TelegramBase):
     """Client of telegram"""
 
     def __init__(
@@ -211,8 +182,8 @@ class Telegram(ITelegram, TelegramBase):
     ) -> None:
         """
         Args:
-            token(str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
-            chat_id(int, str, list): Chat ids for send message.
+            token (str): Telegram bot token. To receive: https://core.telegram.org/bots#6-botfather.
+            chat_id (int, str, list): Chat ids for send message.
             api_url (str, optional): telegram api url.
         """
         super().__init__(token=token, chat_id=chat_id, api_url=api_url)
@@ -226,22 +197,18 @@ class Telegram(ITelegram, TelegramBase):
         params: Optional[Dict] = None,
         body: Optional[Dict] = None,
         files: Optional[Dict] = None,
-    ) -> Optional[Response]:
-        """
-        Send post request.
+    ) -> None:
+        """Send post request.
 
         Args:
-            method_api(str): method of api telegram
-            headers(dict, optional): headers for of request.
-            params(dict, optional): params of request.
-            body(dict, optional): body of request.
-            files(dict, optional): files of request in format ('filename.txt', b'filedata').
-
-        Returns:
-            instance of Response.
+            method_api (str): method of api telegram
+            headers (dict, optional): headers for of request.
+            params (dict, optional): params of request.
+            body (dict, optional): body of request.
+            files (dict, optional): files of request in format ('filename.txt', b'filedata').
         """
         try:
-            response = self._client.post(
+            self._client.post(
                 url=self._base_api_url + method_api,
                 headers=headers,
                 params=params,
@@ -252,8 +219,6 @@ class Telegram(ITelegram, TelegramBase):
             logger.exception("Error. %s", error)
         except Exception:  # noqa
             logger.error("Send message to telegram error.")
-        else:
-            return response
 
     def send_message(
         self,
@@ -264,9 +229,9 @@ class Telegram(ITelegram, TelegramBase):
         """Send message.
 
         Args:
-            msg(str): text of message.
-            disable_notification(bool): True to disable notification of message.
-            disable_web_page_preview(bool): True to disable web preview for links.
+            msg (str): text of message.
+            disable_notification (bool): True to disable notification of message.
+            disable_web_page_preview (bool): True to disable web preview for links.
         """
         method_api = "sendMessage"
 
@@ -294,11 +259,9 @@ class Telegram(ITelegram, TelegramBase):
         Args:
             attach (bytes, str, tuple): file to send. if tuple, then
                 ('filename.txt', b'text of file').
-
             msg (str, optional): text of message.
-            filename(str, optional): filename if attach is string or bytes.
-
-            disable_notification(bool): True to disable notification of message.
+            filename (str, optional): filename if attach is string or bytes.
+            disable_notification (bool): True to disable notification of message.
         """
         method_api = "sendDocument"
         files = self._prepare_attach(attach=attach, filename=filename)
